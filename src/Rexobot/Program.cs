12e72e3cfd51afcd2788d41876c74fb159955c4d
Aux <justin@akitaux.com>
2020-07-19 10:32:57 -0500

    init

---
+++ src/Rexobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rexobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
